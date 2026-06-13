43d01c80d6b86731740d10ae6934862e429d48df
amu0922-cmd <amu0922@gmail.com>
2026-06-13 15:19:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oshikatsu-log-v1';
+const CACHE = 'oshikatsu-log-v10';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
